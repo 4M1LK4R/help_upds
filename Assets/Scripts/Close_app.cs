1699eab4fdadc66639799f590f4c9fc82bcb1a6a
4M1LK4R <amilkar.dominguez@gmail.com>
2017-10-30 21:43:13 -0400

    creacion

---
+++ Assets/Scripts/Close_app.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Close_app : MonoBehaviour {
+
+    public void close_app()
+    {
+        Application.Quit();
+    }
+}
